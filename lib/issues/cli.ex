5141d6572f8797cb9f1521a4e3e0704f545ec0d6
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-10-08 23:02:42 -0700

    add tesla, mint, castore

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -6,7 +6,9 @@ defmodule Issues.CLI do
   @default_count 4
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args()
+    |> process()
   end
 
   @doc """
@@ -37,5 +39,17 @@ defmodule Issues.CLI do
 
   def args_to_internal_representation(_) do
     :help
+  end
+
+  def process(:help) do
+    IO.puts("""
+    usage: issues <user> <project> [count | #{@default_count}]
+    """)
+
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
